1d598e80c3925d5f29fab8328c3ed9d168e36558
Joachim Schiele <js@lastlog.de>
2016-12-28 23:42:39 +0100

    added js/font/templates to pankat

--- server/server.go
+++ server/server.go
@@ -2,9 +2,9 @@ package main
 
 import (
     "github.com/gocraft/web"
-    "fmt"
+//     "fmt"
     "net/http"
-    "strings"
+//     "strings"
     "golang.org/x/net/websocket"
 //     "io"
     "log"
@@ -15,16 +15,7 @@ import (
 // https://github.com/golang-samples/websocket/blob/master/websocket-chat/src/chat/client.go
 
 type Context struct {
-    HelloCount int
-}
-
-func (c *Context) SetHelloCount(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
-    c.HelloCount = 51
-    next(rw, req)
-}
-
-func (c *Context) SayHello(rw web.ResponseWriter, req *web.Request) {
-    fmt.Fprint(rw, strings.Repeat("Hello ", c.HelloCount), "World!")
+//     HelloCount int
 }
 
 // inotify events
@@ -64,11 +55,16 @@ func main() {
         Middleware(web.ShowErrorsMiddleware).       // ...
         //Middleware(web.StaticMiddleware("../output")).
         Middleware(web.StaticMiddleware("../output")). // FIXME hardcoded path
-        Middleware((*Context).SetHelloCount).       // Your own middleware!
         Get("/websocket", func(rw web.ResponseWriter, req *web.Request) {
           websocket.Handler(server.OnConnected).ServeHTTP(rw, req.Request)
         }).
-        Get("/", (*Context).SayHello)               // Add a route
+        Get("/", redirectTo("/index.html"))
+
   http.ListenAndServe("localhost:8080", router)     // Start the server!
 }
 
+func redirectTo(to string) func(web.ResponseWriter, *web.Request) {
+    return func(rw web.ResponseWriter, req *web.Request) {
+       http.Redirect(rw, req.Request, to, http.StatusFound)
+    }
+}
