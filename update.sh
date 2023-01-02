69e9b646a21769e2b1bcb7714fb4b55b2c351fa0
Joachim Schiele <js@lastlog.de>
2023-01-03 00:21:45 +0100

    cleanup of duplicated paths

--- update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-time ./pankat --input documents/ --output output
-#time ./pankat -i documents/ -o output -b lastlog.de/blog -u https://lastlog.de/blog
--- watch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while true; do 
-  inotifywait -r -e modify *; 
-  ./update.sh
-done
