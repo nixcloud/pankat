cdd971390a499befe1c11c2fb0419c1c79eb69fd
Joachim Schiele <js@lastlog.de>
2023-06-17 02:26:23 +0200

    some more functionality

--- cmd/article-gorm/ArticlesDb_test.go
+++ cmd/article-gorm/ArticlesDb_test.go
@@ -9,8 +9,6 @@ import (
 	"gorm.io/gorm"
 )
 
-const longForm = "2006-01-02 15:04"
-
 type Article struct {
 	gorm.Model
 	SrcFileName      string `gorm:"uniqueIndex"`
@@ -57,42 +55,95 @@ func NewArticlesDb() *ArticlesDb {
 	return &ArticlesDb{db: db}
 }
 
-// https://gorm.io/docs/create.html
 func (a *ArticlesDb) Add(article *Article) error {
-	result := a.db.Create(&article)
+	result := a.db.Where("src_file_name = ?", article.SrcFileName).FirstOrCreate(&article)
 	if result.Error != nil {
 		return result.Error
 	}
-	//FIXME needs checking
-	//article.ID          // returns inserted data's primary key
-	//result.Error        // returns error
-	//result.RowsAffected // returns inserted records count
-
-	//result := db.Where("SrcFileName = ?", article.SrcFileName).Updates(&article)
-	//
-	//if result.Error != nil {
-	//	return result.Error
-	//}
 	return nil
 }
 
 func (a *ArticlesDb) Del(SrcFileName string) error {
-	//FIXME needs checking
-
-	result := a.db.Where("srcfilename = ?", SrcFileName).Delete(&Article{})
+	result := a.db.Where("src_file_name = ?", SrcFileName).Delete(&Article{})
 	if result.Error != nil {
-		panic(result.Error)
-	}
-	if result.RowsAffected != 1 {
-		panic("no rows affected")
+		return result.Error
 	}
 	return nil
 }
 
-func TestArticlesDatabase(t *testing.T) {
+func (a *ArticlesDb) All() ([]Article, error) {
+	var articles []Article
+	result := a.db.Order("modification_date DESC").Find(&articles)
+	if result.Error != nil {
+		return []Article{}, nil
+	}
+	return articles, nil
+}
 
+func (a *ArticlesDb) Articles() ([]Article, error) {
+	var articles []Article
+	result := a.db.Order("modification_date DESC").Where("draft = ?", false).Where("special_page = ?", false).Find(&articles)
+	if result.Error != nil {
+		return []Article{}, nil
+	}
+	return articles, nil
+}
+
+func (a *ArticlesDb) MostRecentArticle() (Article, error) {
+	var article Article
+	result := a.db.Order("modification_date DESC").Where("draft = ?", false).Where("special_page = ?", false).First(&article)
+	if result.Error != nil {
+		return Article{}, nil
+	}
+	return article, nil
+}
+
+func (a *ArticlesDb) NextArticle() (Article, error) {
+	//FIXME implement and test these
+	var article Article
+	result := a.db.Order("modification_date DESC").Where("draft = ?", false).Where("special_page = ?", false).First(&article)
+	if result.Error != nil {
+		return Article{}, result.Error
+	}
+	return article, nil
+}
+
+func (a *ArticlesDb) PrevArticle() (Article, error) {
+	//FIXME implement and test these
+	var article Article
+	result := a.db.Order("modification_date DESC").Where("draft = ?", false).Where("special_page = ?", false).First(&article)
+	if result.Error != nil {
+		return Article{}, nil
+	}
+	return article, nil
+}
+
+//FIXME implement and test these
+//func (a *ArticlesDb) NextArticleInSeries(series string) (Article, error) {}
+//func (a *ArticlesDb) PrevArticleInSeries(series string) (Article, error) {}
+
+func (a *ArticlesDb) Drafts() ([]Article, error) {
+	var articles []Article
+	result := a.db.Order("modification_date DESC").Where("draft = ?", true).Find(&articles)
+	if result.Error != nil {
+		panic(result.Error)
+	}
+	return articles, nil
+}
+
+func (a *ArticlesDb) SpecialPages() ([]Article, error) {
+	var articles []Article
+	result := a.db.Order("modification_date DESC").Where("special_page = ?", true).Find(&articles)
+	if result.Error != nil {
+		panic(result.Error)
+	}
+	return articles, nil
+}
+
+func TestArticlesDatabase(t *testing.T) {
 	articlesDb := NewArticlesDb()
 
+	const longForm = "2006-01-02 15:04"
 	time1, _ := time.Parse(longForm, "2019-01-01 00:00")
 	article1 := Article{Title: "foo", ModificationDate: time1, Summary: "foo summary", Tags: []Tag{{Name: "Linux"}, {Name: "Go"}},
 		SrcFileName: "/home/user/documents/foo.mdwn", DstFileName: "/home/user/documents/foo.html"}
@@ -105,6 +156,9 @@ func TestArticlesDatabase(t *testing.T) {
 	time4, _ := time.Parse(longForm, "2024-01-01 00:00")
 	article4 := Article{Draft: true, Title: "draft", ModificationDate: time4, Summary: "draft summary", Tags: []Tag{{Name: "Go"}, {Name: "Linux"}},
 		SrcFileName: "/home/user/documents/mydraft.mdwn", DstFileName: "/home/user/documents/mydraft.html"}
+	time5, _ := time.Parse(longForm, "2024-01-01 00:00")
+	article5 := Article{SpecialPage: true, Title: "draft", ModificationDate: time5,
+		SrcFileName: "/home/user/documents/about.mdwn", DstFileName: "/home/user/documents/about.html"}
 
 	// Insert the article into the database
 	err := articlesDb.Add(&article1)
@@ -123,22 +177,49 @@ func TestArticlesDatabase(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-
-	//// Query the table to verify insertion
-	var articles []Article
-	result := articlesDb.db.Order("modification_date DESC").Find(&articles)
-	if result.Error != nil {
-		panic(result.Error)
+	err = articlesDb.Add(&article5)
+	if err != nil {
+		panic(err)
+	}
+	// try double insert
+	err = articlesDb.Add(&article5)
+	if err != nil {
+		panic(err)
 	}
 
-	assert.True(t, result.Error == nil)
+	all, err := articlesDb.All()
+	assert.True(t, err == nil)
+	assert.Equal(t, len(all), 5)
 
-	////// Query all articles in the db //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//fmt.Println("All Articles in DB:")
-	//for _, a := range articles {
-	//	fmt.Printf("Date: %s, SrcFileName: %s, DstFileName: %s, Title: %s, Tags %v\n",
-	//		a.ModificationDate, a.SrcFileName, a.DstFileName, a.Title, a.Tags)
-	//}
+	allarticles, err := articlesDb.All()
+	assert.True(t, err == nil)
+	assert.Equal(t, len(allarticles), 5)
+
+	drafts, err := articlesDb.Drafts()
+	assert.True(t, err == nil)
+	assert.Equal(t, len(drafts), 1)
+
+	specialpages, err := articlesDb.SpecialPages()
+	assert.True(t, err == nil)
+	assert.Equal(t, len(specialpages), 1)
+
+	mostRecentArticle, err := articlesDb.MostRecentArticle()
+	assert.True(t, err == nil)
+	assert.Equal(t, mostRecentArticle.SrcFileName, "/home/user/documents/bar.mdwn")
+
+	////// Find next/previous article ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// FIXME todo
+	_, err = articlesDb.NextArticle()
+	assert.Error(t, err)
+
+	prevArticle, err := articlesDb.PrevArticle()
+	assert.NotNil(t, err)
+	assert.Equal(t, prevArticle.SrcFileName, "/home/user/documents/ba33r.mdwn")
+
+	//nextArticle, err := articlesDb.NextArticle()
+	//assert.Error(t, err)
+	//assert.Equal(t, nextArticle.SrcFileName, "/home/user/documents/bar.mdwn")
+
 	//// FIXME broken, []Tag is empty
 
 	//// Query articles that are not drafts and sort by ModificationDate /////////////////////////////////////////////////////////////////////////////////////////
@@ -152,9 +233,6 @@ func TestArticlesDatabase(t *testing.T) {
 	//	fmt.Printf("Date: %s, SrcFileName: %s, DstFileName: %s, Title: %s\n",
 	//		a.ModificationDate, a.SrcFileName, a.DstFileName, a.Title)
 	//}
-
-	////// Find next/previous article ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// FIXME todo
 
 	// update item ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//article4update := Article{Title: "done", ModificationDate: time4, Summary: "done summary", Tags: []Tag{{Name: "Linux"}, {Name: "Go"}},
@@ -179,4 +257,7 @@ func TestArticlesDatabase(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+	all2, err := articlesDb.All()
+	assert.True(t, err == nil)
+	assert.Equal(t, len(all2), 4)
 }
